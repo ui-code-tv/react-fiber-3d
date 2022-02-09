5c207b75b3027b45870fc00c5af4dfd91ed1d2b8
ui-cod-tv <uicodetv@googlemail.com>
2022-02-09 15:01:07 +0000

    first rev

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState,useRef} from "react"
+import {Canvas, useFrame} from "@react-three/fiber"
+import { useSpring, animated } from '@react-spring/three'
+
+
+function Cube(props) {
+
+  const mesh=useRef()
+  useFrame ( ()=> (mesh.current.rotation.x += 0.01))
+  const [hovered,setHover] = useState(false)
+  const [active,setActive] = useState(false)
+  const { scale } = useSpring({ scale: active ? 1.5 : 1 })
+
+  return (
+          <animated.mesh ref={mesh}
+            onPointerOver={ (event)=> setHover(true)}
+            onPointerOut={(event)=> setHover(false)}
+            onClick = {(event)=> setActive(!active)}
+            scale = { scale}
+            {...props}
+          >
+             <boxGeometry args={[2,2,2]}/>
+             <meshStandardMaterial color={hovered ? "hotpink" : "orange"}/> 
+
+          </animated.mesh>
+
+  )
+
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Canvas>
+     <ambientLight />
+     <pointLight position={[10,10,10]} />
+     <Cube position={[-2.2,0,0]}/>
+     <Cube position={[2.2,0,0]}/>
+
+   </Canvas>
   );
 }
 
